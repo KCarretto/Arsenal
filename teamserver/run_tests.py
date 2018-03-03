513dfd8cf7f1358b5865f694abe19ae0a4c7be61
Kyle Carretto <kcarretto@gmail.com>
2018-03-03 14:11:07 -0500

    Fixed run_tests.py to exit with non 0 code if unit tests fail

--- teamserver/run_tests.py
+++ teamserver/run_tests.py
@@ -1,10 +1,12 @@
 """
     This module will run all unit test cases.
 """
+import sys
 import unittest
 
 LOADER = unittest.TestLoader()
 SUITE = LOADER.discover('tests/', pattern='*_test.py')
 
 RUNNER = unittest.TextTestRunner()
-RUNNER.run(SUITE)
+ret = not RUNNER.run(SUITE).wasSuccessful()
+sys.exit(ret)
