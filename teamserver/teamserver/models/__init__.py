95f38fc7dd47d724ad4817cde523985df731bd0c
Kyle Carretto <kcarretto@gmail.com>
2018-03-07 04:48:17 -0500

    Moved GroupAction model to separate document

--- teamserver/teamserver/models/__init__.py
+++ teamserver/teamserver/models/__init__.py
@@ -2,7 +2,8 @@
 This package contains all database models, and convinient functions
 for manipulating them.
 """
-from .action import Action, GroupAction, Response
+from .action import Action, Response
+from .group_action import GroupAction
 from .group import Group, GroupAutomemberRule
 from .session import Session, SessionHistory
 from .target import Target, Credential
