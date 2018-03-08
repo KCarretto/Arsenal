665fbdf9f0b02e482575cfd4b898a1fac301b855
Kyle Carretto <kcarretto@gmail.com>
2018-03-07 21:45:52 -0500

    Set up basic pipeline for travis to deploy to EC2

--- teamserver/run.py
+++ teamserver/run.py
@@ -1,10 +1,13 @@
 """
     This runs the teamserver using a Flask webserver (not recommended for production use).
 """
+import sys
+print (sys.path)
 
 from teamserver import create_app
 
+TEAMSERVER = create_app()
+
 if __name__ == '__main__':
-    TEAMSERVER = create_app()
     TEAMSERVER.run(debug=False)
 
