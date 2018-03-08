b05684c500ac7a2faaf5650433ec4b430bf7db4f
Kyle Carretto <kcarretto@gmail.com>
2018-03-07 21:49:02 -0500

    Fixed pylint error

--- teamserver/run.py
+++ teamserver/run.py
@@ -1,9 +1,6 @@
 """
     This runs the teamserver using a Flask webserver (not recommended for production use).
 """
-import sys
-print (sys.path)
-
 from teamserver import create_app
 
 TEAMSERVER = create_app()
