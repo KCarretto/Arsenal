0ea9dc7ecbfa1f246bb1783e7f975bcbff869c8c
Kyle Carretto <kcarretto@gmail.com>
2018-04-04 22:47:16 -0400

    Attempted to fix imports

--- teamserver/teamserver/events/worker.py
+++ teamserver/teamserver/events/worker.py
@@ -5,27 +5,28 @@ import time
 import requests
 
 from celery import Celery
-from ..config import CELERY_MAIN_NAME, CELERY_RESULT_BACKEND, CELERY_BROKER_URL
-from ..models import Webhook
 
-CELERY = Celery(
+from teamserver.config import CELERY_MAIN_NAME, CELERY_RESULT_BACKEND, CELERY_BROKER_URL
+from teamserver.models import Webhook
+
+app = Celery( # pylint: disable=invalid-name
     CELERY_MAIN_NAME,
     backend=CELERY_RESULT_BACKEND,
     broker=CELERY_BROKER_URL,
 )
 
-@CELERY.task
+@app.task
 def trigger_event(**kwargs):
     """
     Trigger an event, and notify subscribers.
     """
     print("Triggering event")
     time.sleep(15)
-    event = kwargs.get('event')
-    subscribers = Webhook.get_subscribers(event)
+    #event = kwargs.get('event')
+    #subscribers = Webhook.get_subscribers(event)
 
-    if subscribers:
-        kwargs['subscribers'] = [subscriber.document for subscriber in subscribers]
+    #if subscribers:
+    #    kwargs['subscribers'] = [subscriber.document for subscriber in subscribers]
 
     requests.post('http://129.21.106.18', json=kwargs)
 
