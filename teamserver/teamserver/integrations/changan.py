4c0a0bfb8a19e2d9c4ec7c36eacf9a37e9e148ec
koalatea <koalateac@gmail.com>
2018-04-21 02:50:25 -0400

    changan integration with fixing of documentation

--- teamserver/teamserver/integrations/changan.py
+++ teamserver/teamserver/integrations/changan.py
@@ -3,6 +3,7 @@
     https://github.com/koalatea/changan
     Author: Ryan Whittier
 """
+import requests
 from .integration import Integration
 
 class ChanganIntegration(Integration): #pylint: disable=too-few-public-methods
@@ -15,7 +16,7 @@ class ChanganIntegration(Integration): #pylint: disable=too-few-public-methods
         Initialize that integration.
         """
         self.config = config
-        self.url = config.get("URL", "https://changan.local:8080/generic")
+        self.url = config.get("URL", "https://changan.koalatea.me:8080/")
 
     def __str__(self):
         """
@@ -27,21 +28,46 @@ class ChanganIntegration(Integration): #pylint: disable=too-few-public-methods
         """
         nothing right now
         """
-        pass
+        try:
+            interfaces = []
+            for interface in event_data['facts']['interfaces']:
+                my_interface = {}
+                my_interface['name'] = interface['name']
+                my_interface['mac'] = interface['mac_addr']
+                my_interface['ips'] = []
+                for ip_addr in interface['ip_addrs']:
+                    my_interface['ips'].append(ip_addr.split('/')[0])
+                interfaces.append(my_interface)
+            add_client_data = {'device_name': event_data['name'], 'interface': my_interface}
+            requests.put('{}api/v1/devices'.format(self.url), json=add_client_data, veriy=False)
+        except: #pylint: disable=bare-except
+            return
 
     def handle_target_name_change(self, event_data):
         """
         nothing right now
         """
-        pass
+        try:
+            query_data = {'device_name': event_data['old_name']}
+            resp = requests.get('{}api/v1/devices'.format(self.url), json=query_data, verify=False)
+            device_id = resp.json().get('device_id', '')
+            if device_id:
+                change_data = {'device_id': device_id, 'device_name': event_data['new_name']}
+                resp = requests.post('{}api/v1/devices'.format(self.url), json=change_data,
+                                     verify=False)
+        except: #pylint: disable=bare-except
+            return
 
     def run(self, event_data, **kwargs):
         """
         Post an update to changan
+        if not self.config.get('enabled', False):
         """
-        event = event_data.get("event")
 
         handled_events = {
-            'one_event': self.handle_create_target,
-            'two_event': self.handle_target_name_change,
+            'target_create': self.handle_create_target,
+            'target_rename': self.handle_target_name_change,
         }
+        method = handled_events.get(event_data('event', ''))
+        if method and callable(method):
+            method(event_data)
