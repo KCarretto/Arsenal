4d75298ecfede95b9717e26e9d8a3cc076009283
Kyle Carretto <kcarretto@gmail.com>
2019-02-20 19:54:53 -0800

    Making it better <3 (temporarily)

--- teamserver/teamserver/__init__.py
+++ teamserver/teamserver/__init__.py
@@ -2,16 +2,41 @@
     Imports
 """
 import sys
+import logging
+from logging.config import dictConfig
+from typing import Dict, Optional
 
 from flask import Flask
-
 from flask_mongoengine import MongoEngine
 from mongoengine import connect, MongoEngineConnectionError
 
+from .config import MODE
 from .config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
 from .config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from .integrations.integration import Integration
 
 DB = MongoEngine()
+
+
+def _configure_logging(config: Optional[Dict] = None) -> None:
+    opts = {
+        "version": 1,
+        "formatters": {
+            "default": {"format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"}
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+    if config:
+        opts.update(config)
+    dictConfig(opts)
+
 
 def create_app(**config_overrides):
     """
@@ -20,33 +45,54 @@ def create_app(**config_overrides):
     """
     app = Flask(__name__)
 
-    # Initialize configuration
-    app.config.from_object('teamserver.config')
-    app.config['MONGODB_SETTINGS'] = {
-        'db': DB_NAME,
-        'host': DB_HOST,
-        'port': DB_PORT
-    }
-    if DB_USER and DB_PASS:
-        app.config['MONGODB_SETTINGS']['username'] = DB_USER
-        app.config['MONGODB_SETTINGS']['password'] = DB_PASS
+    # Initialize logging
+    _configure_logging()
 
-    app.config['CELERY_BROKER_URL'] = CELERY_BROKER_URL
-    app.config['CELERY_RESULT_BACKEND'] = CELERY_RESULT_BACKEND
+    # Initialize configuration
+    app.config.from_object("teamserver.config")
+    app.config["MODE"] = MODE
+
+    app.config["MONGODB_SETTINGS"] = {"db": DB_NAME, "host": DB_HOST, "port": DB_PORT}
+    if DB_USER and DB_PASS:
+        app.config["MONGODB_SETTINGS"]["username"] = DB_USER
+        app.config["MONGODB_SETTINGS"]["password"] = DB_PASS
+
+    app.config["CELERY_BROKER_URL"] = CELERY_BROKER_URL
+    app.config["CELERY_RESULT_BACKEND"] = CELERY_RESULT_BACKEND
 
     # Override configuration options
     app.config.update(config_overrides)
+
+    # Initialize DEBUG
+    if MODE.upper() == "DEBUG":
+        # Enable debug logging
+        app.logger.setLevel(logging.DEBUG)
+
+        # Enable profiling
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        app.config["PROFILE"] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[50])
+
+        # Enable mongodb debug toolbar
+        from flask_debugtoolbar import DebugToolbarExtension
+
+        app.config["DEBUG_TB_PANELS"] = ["flask_mongoengine.panels.MongoDebugPanel"]
+        app.debug_toolbar = DebugToolbarExtension(app)
+    else:
+        app.logger.setLevel(logging.WARNING)
 
     # Initialize the database
     try:
         DB.init_app(app)
     except MongoEngineConnectionError as conn_err:
         print(conn_err)
-        sys.exit('Could not connect to database.')
+        sys.exit("Could not connect to database.")
 
     # Import endpoints
     from teamserver.router import API
+
     app.register_blueprint(API)
 
-
+    app.logger.info(f"Initialized Arsenal Teamserver [{MODE}]")
     return app
--- teamserver/teamserver/config.py
+++ teamserver/teamserver/config.py
@@ -3,20 +3,21 @@
 """
 import os
 
+# GLOBAL SETTINGS
+MODE = os.environ.get("MODE", "PROD")  # production or debug
+
 # CELERY SETTINGS
-CELERY_MAIN_NAME = 'arsenal'
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://localhost:5672')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
-CELERY_BROKER_TRANSPORT = {
-    'region': 'us-west-2'
-}
+CELERY_MAIN_NAME = "arsenal"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5672")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+CELERY_BROKER_TRANSPORT = {"region": "us-west-2"}
 
 # DATABASE CONNECTION SETTINGS
-DB_NAME = os.environ.get('DB_NAME', 'arsenal_sample')
-DB_HOST = os.environ.get('DB_HOST', 'localhost')
-DB_PORT = int(os.environ.get('DB_PORT', 27017))
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
+DB_NAME = os.environ.get("DB_NAME", "arsenal_sample")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_PORT = int(os.environ.get("DB_PORT", 27017))
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
 
 # DATABASE DOCUMENT SETTINGS
 MAX_STR_LEN = 500
@@ -24,84 +25,74 @@ MAX_BIGSTR_LEN = 10000
 MAX_RESULTS = 500
 
 # DATABASE COLLECTION SETTINGS
-COLLECTION_ACTIONS = 'actions'
-COLLECTION_GROUP_ACTIONS = 'group_actions'
-COLLECTION_GROUPS = 'groups'
-COLLECTION_SESSIONS = 'sessions'
-COLLECTION_SESSION_HISTORIES = 'session_histories'
-COLLECTION_TARGETS = 'targets'
-COLLECTION_TARGET_CREDS = 'target_creds'
-COLLECTION_LOGS = 'logs'
-COLLECTION_AGENTS = 'agents'
-COLLECTION_USERS = 'users'
-COLLECTION_ROLES = 'roles'
-COLLECTION_APIKEYS = 'api_keys'
-COLLECTION_WEBHOOKS = 'webhooks'
+COLLECTION_ACTIONS = "actions"
+COLLECTION_GROUP_ACTIONS = "group_actions"
+COLLECTION_GROUPS = "groups"
+COLLECTION_SESSIONS = "sessions"
+COLLECTION_SESSION_HISTORIES = "session_histories"
+COLLECTION_TARGETS = "targets"
+COLLECTION_TARGET_CREDS = "target_creds"
+COLLECTION_LOGS = "logs"
+COLLECTION_AGENTS = "agents"
+COLLECTION_USERS = "users"
+COLLECTION_ROLES = "roles"
+COLLECTION_APIKEYS = "api_keys"
+COLLECTION_WEBHOOKS = "webhooks"
 
 # LOG SETTINGS
-APPLICATION = 'teamserver-internal'
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
-LOG_LEVELS = {
-    'DEBUG': 0,
-    'INFO': 1,
-    'WARN': 2,
-    'CRIT': 3,
-    'FATAL': 4
-}
+APPLICATION = "teamserver-internal"
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+LOG_LEVELS = {"DEBUG": 0, "INFO": 1, "WARN": 2, "CRIT": 3, "FATAL": 4}
 
 # AGENT SETTINGS
-DEFAULT_AGENT_SERVERS = ['http://redteam.com']
+DEFAULT_AGENT_SERVERS = ["http://redteam.com"]
 DEFAULT_AGENT_INTERVAL = 60
 DEFAULT_AGENT_INTERVAL_DELTA = 15
 DEFAULT_AGENT_CONFIG_DICT = {}
-DEFAULT_SUBSET = 'all'
+DEFAULT_SUBSET = "all"
 
 # SESSION SETTINGS
 SESSION_CHECK_THRESHOLD = 5
 SESSION_CHECK_MODIFIER = 1.5
 SESSION_ARCHIVE_MODIFIER = 2
-SESSION_STATUSES = {
-    'active': 'active',
-    'missing': 'missing',
-    'inactive': 'inactive'
-}
+SESSION_STATUSES = {"active": "active", "missing": "missing", "inactive": "inactive"}
 
 # ACTION SETTINGS
 ACTION_STALE_THRESHOLD = 900
 ACTION_STATUSES = {
-    'cancelled': 'cancelled',
-    'queued': 'queued',
-    'sent': 'sent',
-    'failing': 'failing',
-    'failed': 'failed',
-    'error': 'error',
-    'complete': 'complete',
-    'stale': 'stale',
+    "cancelled": "cancelled",
+    "queued": "queued",
+    "sent": "sent",
+    "failing": "failing",
+    "failed": "failed",
+    "error": "error",
+    "complete": "complete",
+    "stale": "stale",
 }
 GROUP_ACTION_STATUSES = {
-    'cancelled': 'cancelled',
-    'queued': 'queued',
-    'in progress': 'in progress',
-    'mixed success': 'mixed success',
-    'success': 'success',
-    'stale': 'stale',
-    'failed': 'failed',
+    "cancelled": "cancelled",
+    "queued": "queued",
+    "in progress": "in progress",
+    "mixed success": "mixed success",
+    "success": "success",
+    "stale": "stale",
+    "failed": "failed",
 }
 
 ACTION_TYPES = {
-    'config': 0,
-    'exec': 1,
-    'spawn': 2,
-    'timed_exec': 3,
-    'timed_spawn': 4,
-    'upload': 5,
-    'download': 6,
-    'gather': 7,
-    'reset': 999
+    "config": 0,
+    "exec": 1,
+    "spawn": 2,
+    "timed_exec": 3,
+    "timed_spawn": 4,
+    "upload": 5,
+    "download": 6,
+    "gather": 7,
+    "reset": 999,
 }
 
 # AUTH SETTINGS
-API_KEY_SALT = os.environ.get('API_KEY_SALT', "ROBO4PRESIDENT")
+API_KEY_SALT = os.environ.get("API_KEY_SALT", "ROBO4PRESIDENT")
 HASH_TIME_PARAM = 16
 HASH_MEMORY_PARAM = 10
 HASH_PARALLELIZATION_PARAM = 2
@@ -111,7 +102,6 @@ CONNECT_TIMEOUT = 10.0
 READ_TIMEOUT = 30.0
 
 
-
 # INTEGRATION SETTINGS
 def read_api_key(api_key_file):
     """
@@ -119,26 +109,12 @@ def read_api_key(api_key_file):
     """
     api_key = None
     if api_key_file and os.path.exists(api_key_file):
-        with open(api_key_file, 'r') as keyfile:
-            api_key = keyfile.readlines()[0].strip().strip('\n')
+        with open(api_key_file, "r") as keyfile:
+            api_key = keyfile.readlines()[0].strip().strip("\n")
 
     return api_key
 
-INTEGRATIONS = {
-    'SLACK_CONFIG': {
-        'enabled': os.environ.get('SLACK_ENABLED', False),
-        'API_TOKEN': os.environ.get('SLACK_API_TOKEN', read_api_key('.slack_api')),
-        'TIMEOUT': int(os.environ.get('SLACK_TIMEOUT', 10)),
 
-        'ERROR_CHANNEL': os.environ.get('SLACK_ERROR_CHANNEL', 'notifications'),
-        'ACTION_CHANNEL': os.environ.get('SLACK_ACTION_CHANNEL', 'notifications'),
-    },
-    'PWNBOARD_CONFIG': {
-        'enabled': os.environ.get('PWNBOARD_ENABLED', False),
-        'URL': os.environ.get('PWNBOARD_URL', 'https://pwnboard.local/generic')
-    },
-    'CHANGAN_CONFIG': {
-        'enabled': os.environ.get('CHANGAN_ENABLED', False),
-        'URL': os.environ.get('CHANGAN_URL', 'https://changan.koalatea.me:8080/')
-    }
+INTEGRATIONS = {
+    "PWNBOARD_CONFIG": {"URL": os.environ.get("PWNBOARD_URL", "https://pwnboard.local/generic")}
 }
