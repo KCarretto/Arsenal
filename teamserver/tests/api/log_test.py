ff6f7b305fa4ddb6518dfde4c7f882a01995312c
Kyle Carretto <kcarretto@gmail.com>
2019-02-20 21:21:35 -0800

    Logging goes to flask logger now

--- teamserver/tests/api/log_test.py
+++ teamserver/tests/api/log_test.py
@@ -1,58 +1,60 @@
-"""
-    This module tests basic functionality of the log API.
-"""
-from uuid import uuid4
+# NOTE: Logging now uses flask app logger
 
-import sys
-import time
-import unittest
+# """
+#     This module tests basic functionality of the log API.
+# """
+# from uuid import uuid4
 
-try:
-    from testutils import BaseTest, Database, APIClient
-except ModuleNotFoundError:
-    # Configure path to start at teamserver module
-    from os.path import dirname, abspath
-    sys.path.append(abspath(dirname(dirname(dirname(abspath(__file__))))))
-    from tests.testutils import BaseTest, Database, APIClient
+# import sys
+# import time
+# import unittest
 
-class LogAPITest(BaseTest):
-    """
-    This class is used to test the Log API functions.
-    """
-    def test_create(self):
-        """
-        Test the CreateLog API function.
-        """
-        app = 'TEST APP {}'.format(uuid4())
-        data = APIClient.create_log(
-            self.client,
-            'TEST APP',
-            'FATAL',
-            'Testing CreateLog API method'
-        )
-        self.assertEqual(data['error'], False)
+# try:
+#     from testutils import BaseTest, Database, APIClient
+# except ModuleNotFoundError:
+#     # Configure path to start at teamserver module
+#     from os.path import dirname, abspath
+#     sys.path.append(abspath(dirname(dirname(dirname(abspath(__file__))))))
+#     from tests.testutils import BaseTest, Database, APIClient
 
-        for entry in Database.list_logs(app):
-            self.assertEqual(entry.message, 'Testing CreateLog API method')
+# class LogAPITest(BaseTest):
+#     """
+#     This class is used to test the Log API functions.
+#     """
+#     def test_create(self):
+#         """
+#         Test the CreateLog API function.
+#         """
+#         app = 'TEST APP {}'.format(uuid4())
+#         data = APIClient.create_log(
+#             self.client,
+#             'TEST APP',
+#             'FATAL',
+#             'Testing CreateLog API method'
+#         )
+#         self.assertEqual(data['error'], False)
 
-    def test_list(self):
-        """
-        Test the ListLogs API function.
-        """
-        Database.create_log(0, 'NOSEE', 'FATAL', 'NOSEE')
-        Database.create_log(time.time(), 'WESEE', 'FATAL', 'WESEE')
-        archived_log = Database.create_log(time.time(), 'ARCHIVED', 'FATAL', 'ARCHIVED')
-        archived_log.archived = True
-        archived_log.save()
+#         for entry in Database.list_logs(app):
+#             self.assertEqual(entry.message, 'Testing CreateLog API method')
 
-        for entry in APIClient.list_logs(self.client, False, 10)['logs']:
-            self.assertNotEqual(entry['application'], 'NOSEE')
-            self.assertNotEqual(entry['application'], 'ARCHIVED')
+#     def test_list(self):
+#         """
+#         Test the ListLogs API function.
+#         """
+#         Database.create_log(0, 'NOSEE', 'FATAL', 'NOSEE')
+#         Database.create_log(time.time(), 'WESEE', 'FATAL', 'WESEE')
+#         archived_log = Database.create_log(time.time(), 'ARCHIVED', 'FATAL', 'ARCHIVED')
+#         archived_log.archived = True
+#         archived_log.save()
 
-        logs = [entry['application'] for entry in APIClient.list_logs(self.client, True)['logs']]
-        self.assertIn('NOSEE', logs)
-        self.assertIn('ARCHIVED', logs)
+#         for entry in APIClient.list_logs(self.client, False, 10)['logs']:
+#             self.assertNotEqual(entry['application'], 'NOSEE')
+#             self.assertNotEqual(entry['application'], 'ARCHIVED')
+
+#         logs = [entry['application'] for entry in APIClient.list_logs(self.client, True)['logs']]
+#         self.assertIn('NOSEE', logs)
+#         self.assertIn('ARCHIVED', logs)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
--- teamserver/tests/general/status_test.py
+++ teamserver/tests/general/status_test.py
@@ -7,23 +7,27 @@ import sys
 import unittest
 
 try:
-    from testutils.test_cases import BaseTest #pylint: disable=no-name-in-module
+    from testutils.test_cases import BaseTest  # pylint: disable=no-name-in-module
 except ModuleNotFoundError:
     from os.path import abspath, dirname
-    sys.path.append(abspath(dirname(dirname(dirname(abspath(__file__))))))
-    from tests.testutils.test_cases import BaseTest #pylint: disable=no-name-in-module
 
-class StatusTest(BaseTest): # pylint: disable=too-few-public-methods
+    sys.path.append(abspath(dirname(dirname(dirname(abspath(__file__))))))
+    from tests.testutils.test_cases import BaseTest  # pylint: disable=no-name-in-module
+
+
+class StatusTest(BaseTest):  # pylint: disable=too-few-public-methods
     """
     This class is used to test the flask application's /status route.
     """
+
     def test_status(self):
         """
         This test will pass if the /status endpoint returns no errors.
         """
-        resp = self.client.get('/status')
-        self.assertIn('"error": false', str(resp.data))
+        resp = self.client.get("/status")
+        self.assertIn('"error":false', str(resp.data))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
 
